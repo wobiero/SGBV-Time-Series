0aa4a7898d3ea153a6bfc4f3b2b36dc9e254bcfc
wobiero <41239437+wobiero@users.noreply.github.com>
2021-12-31 08:18:42 -0500

    Update sgbv_time_series.py

--- sgbv_time_series.py
+++ sgbv_time_series.py
@@ -1,5 +1,8 @@
-import pandas as pd
-from pandas.api.types import is_list_like
+import warnings 
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    import pandas as pd
+
 import numpy as np
 import streamlit as st
 from streamlit_disqus import st_disqus
